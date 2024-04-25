67602e193d7d774685b06b29189423672f6851a6
Benson Oluwole Elvis <elviswole@yahoo.com>
2024-04-25 19:17:28 +0100

    Added all Code in one class

--- Sources/iccfanpassportiosSDK/iccfanpassportlauncher.swift
+++ Sources/iccfanpassportiosSDK/iccfanpassportlauncher.swift
@@ -5,18 +5,15 @@
 //  Created by Computer on 4/25/24.
 //
 
+// ICCWebView.swift
+
 import Foundation
 import UIKit
 import WebKit
 
-
-
-// ICCWebViewLaunch.swift
-
 public class ICCWebView: UIViewController, WKNavigationDelegate {
     public var webView: WKWebView!
-    private var authManager: AuthManager = AuthManager()
-    public let baseUrlString = "https://icc-fan-passport-staging.vercel.app/"
+    private var baseUrlString = "https://icc-fan-passport-staging.vercel.app/"
     
     public var authToken: String
     public var name: String
@@ -46,8 +43,9 @@ public class ICCWebView: UIViewController, WKNavigationDelegate {
         webView.navigationDelegate = self
         view.addSubview(webView)
     }
+    
     public func startSDKOperations() {
-        authManager.StartSDK(authToken: authToken, name: name, email: email, username: username) { encryptedToken in
+        encryptAuthToken(authToken: authToken) { encryptedToken in
             let urlString = "\(self.baseUrlString)?passport_access=\(encryptedToken)"
             if let url = URL(string: urlString) {
                 self.webView.load(URLRequest(url: url))
@@ -57,18 +55,40 @@ public class ICCWebView: UIViewController, WKNavigationDelegate {
         }
     }
 
-    
-//    private func triggerSDKOperations() {
-//        authManager.triggerSDK(authToken: authToken, name: name, email: email, username: username) { encryptedToken in
-//            let urlString = "\(baseUrlString)?passport_access=\(encryptedToken)"
-//            if let url = URL(string: urlString) {
-//                webView.load(URLRequest(url: url))
-//            } else {
-//                print("Error: Invalid URL")
-//            }
-//        }
-//    }
+    private func encryptAuthToken(authToken: String, completion: @escaping (String) -> Void) {
+        // Prepare the request
+        let url = URL(string: "http://icc-fan-passport-stg-env.eba-tptrhhya.us-east-1.elasticbeanstalk.com/auth/encode")!
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        // Prepare the request body
+        let requestBody: [String: String] = [
+            "authToken": authToken
+        ]
+        let jsonData = try! JSONSerialization.data(withJSONObject: requestBody)
+        request.httpBody = jsonData
+        
+        // Make the request
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else {
+                print("Error: \(error?.localizedDescription ?? "Unknown error")")
+                return
+            }
+            
+            // Parse the response
+            if let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: String],
+               let encryptedToken = json["token"] {
+                // Call completion handler with encrypted token
+                completion(encryptedToken)
+            } else {
+                print("Error: Unable to parse response")
+            }
+        }
+        task.resume()
+    }
     
     // WKNavigationDelegate methods...
 }
+
 
