6370ceafb8910f6bf54b052ee52b97d1075d0112
Benson Oluwole Elvis <elviswole@yahoo.com>
2024-05-27 04:22:46 +0100

    Added deeplinks, Wallet creation sorted and open the SDK without sign in

--- Sources/iccfanpassportiosSDK/iccfanpassportlauncher.swift
+++ Sources/iccfanpassportiosSDK/iccfanpassportlauncher.swift
@@ -10,18 +10,30 @@ import UIKit
 import WebKit
 import SafariServices
 
-public class ICCWebView: UIViewController, WKNavigationDelegate, WKScriptMessageHandler, SFSafariViewControllerDelegate {
+public class ICCWebView: UIViewController, WKNavigationDelegate, WKScriptMessageHandler, SFSafariViewControllerDelegate,UIWebViewDelegate {
+    
+    private var urlList: [String] = []
+    private var currentIndex: Int = 0
+    
     public var webView: WKWebView!
     private var baseUrlString: String
     private var UrlStringMint: String
     private var UrlStringEncode: String
+    private var deepLinkURLFantasy: String
     private var activityIndicator: UIActivityIndicatorView!
+    private var backgroundImageView: UIImageView!
     
-    public var authToken: String
-    public var name: String
-    public var email: String
+    public var authToken: String?
+    public var name: String?
+    public var email: String?
     public var initialEntryPoint: PassportEntryPoint
-    public var navigateToICCAction: ((UIViewController) -> Void)? // Callback closure
+   
+    public typealias NavigateToICCAction = (UIViewController) -> Void  // Define callback type for navigation
+    public var navigateToICCAction: NavigateToICCAction?  // Property to store navigation callback
+
+    public typealias SignInWithIccCompletion = (Bool) -> Void  // Define callback type for sign-in
+    public var signInWithIccCompletion: SignInWithIccCompletion?  // Property to store sign-in callback
+
 
     public init(authToken: String, name: String, email: String, initialEntryPoint: PassportEntryPoint, environment: Environment) {
         self.authToken = authToken
@@ -31,12 +43,14 @@ public class ICCWebView: UIViewController, WKNavigationDelegate, WKScriptMessage
         switch environment {
         case .development:
             self.baseUrlString = "https://icc-fan-passport-staging.vercel.app/"
-            self.UrlStringMint = "https://wallet.mintbase.xyz/connect?theme=icc&success_url=iccdev://mintbase.xyz"
+            self.UrlStringMint = "https://testnet.wallet.mintbase.xyz/connect?theme=icc&success_url=iccdev://mintbase.xyz"
             self.UrlStringEncode = "https://icc-fan-passport-stg-api.insomnialabs.xyz/auth/encode"
+            self.deepLinkURLFantasy = "iccdev://react-fe-en.icc-dev.deltatre.digital/fantasy-game"
         case .production:
             self.baseUrlString = "https://fanpassport.icc-cricket.com/"
             self.UrlStringMint = "https://wallet.mintbase.xyz/connect?theme=icc?success_url=icc://mintbase.xyz"
             self.UrlStringEncode = "https://passport-api.icc-cricket.com/"
+            self.deepLinkURLFantasy = "icc://www.icc-cricket.com/fantasy-game"
         }
         super.init(nibName: nil, bundle: nil)
     }
@@ -54,12 +68,20 @@ public class ICCWebView: UIViewController, WKNavigationDelegate, WKScriptMessage
     public override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         setupWebView()
-        startSDKOperations(entryPoint: initialEntryPoint)
+        setupBackgroundImageView()
+        setupActivityIndicator()
+        if let authToken = self.authToken, !authToken.isEmpty {
+            startSDKOperations(entryPoint: initialEntryPoint)
+        } else {
+            loadURL(baseUrlString)
+        }
+        //startSDKOperations(entryPoint: initialEntryPoint)
     }
 
     public func setupWebView() {
-        webView = WKWebView(frame: view.bounds)
+        webView = WKWebView()
         webView.navigationDelegate = self
+        webView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(webView)
         
         // Enable JavaScript
@@ -67,54 +89,177 @@ public class ICCWebView: UIViewController, WKNavigationDelegate, WKScriptMessage
         
         // Add script message handler for 'navigateToIcc'
         webView.configuration.userContentController.add(self, name: "navigateToIcc")
+        
+        // Set up Auto Layout constraints to make the webView full screen
+        NSLayoutConstraint.activate([
+            webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            webView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            webView.topAnchor.constraint(equalTo: view.topAnchor),
+            webView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
     }
 
+    public func setupBackgroundImageView() {
+        // Initialize the background image view
+        backgroundImageView = UIImageView(image: UIImage(named: "loadingpage.png"))
+        backgroundImageView.contentMode = .scaleAspectFill
+        backgroundImageView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(backgroundImageView)
+        
+        // Set up Auto Layout constraints to make the background image view full screen
+        NSLayoutConstraint.activate([
+            backgroundImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            backgroundImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            backgroundImageView.topAnchor.constraint(equalTo: view.topAnchor),
+            backgroundImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+
+    public func setupActivityIndicator() {
+        activityIndicator = UIActivityIndicatorView(style: .large)
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(activityIndicator)
+        
+        // Center the activity indicator in the view
+        NSLayoutConstraint.activate([
+            activityIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            activityIndicator.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+
+    
     public func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-        // Inject JavaScript to handle 'navigate-to-icc' event
+        // Inject JavaScript to handle multiple events
         let script = """
             window.addEventListener('navigate-to-icc', function() {
                 window.webkit.messageHandlers.navigateToIcc.postMessage(null);
             });
+            window.addEventListener('go-to-fantasy', function() {
+                  window.webkit.messageHandlers.goToFantasy.postMessage(null);
+                });
+            window.addEventListener('sign-in-with-icc', function() {
+                  window.webkit.messageHandlers.signInWithIcc.postMessage(null);
+                });
         """
+
         webView.evaluateJavaScript(script, completionHandler: nil)
+
+        activityIndicator.stopAnimating()
+        backgroundImageView.isHidden = true // Hide the background image when loading finishes
+        
     }
 
+    public func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        activityIndicator.startAnimating()
+        backgroundImageView.isHidden = false // Show the background image when loading starts
+    }
+
+    public func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+        activityIndicator.stopAnimating()
+        backgroundImageView.isHidden = true // Hide the background image if loading fails
+    }
+
+
+    
     public func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
-        if message.name == "navigateToIcc" {
-            // Handle the 'navigate-to-icc' event here
+        switch message.name {
+        case "navigateToIcc":
             print("Received 'navigate-to-icc' event")
-            // Call the callback action
             navigateToICCAction?(self)
+        case "goToFantasy":
+            print("Received 'go-to-fantasy' event")
+            // Call your callback action for event-name-1 here
+            openDeepLink(urlString: deepLinkURLFantasy)
+            print("Fantasy")
+        case "signInWithIcc":
+            print("Received 'sign-in-with-icc' event")
+            // Call your callback action for event-name-2 here
+            signInWithIccCompletion?(true)
+        default:
+            print("Received unknown event: \(message.name)")
         }
     }
     
+    func openDeepLink(urlString: String) {
+        showPopup(title: "Fantasy", message: "I got to fantasy")
+      guard let url = URL(string: urlString) else {
+        print("Error: Invalid deep link URL")
+        return
+      }
+        
+
+      // Open the deep link using your preferred method (adapt as needed)
+      UIApplication.shared.open(url)
+    }
+    
     public func startSDKOperations(entryPoint: PassportEntryPoint, accountid: String? = nil, publickey: String? = nil) {
-        encryptAuthToken(authToken: authToken) { encryptedToken in
-            DispatchQueue.main.async {
-                var urlString: String
+        
+        if let accountId = accountid, let publicKey = publickey, !accountId.isEmpty {
+            let userDefaults = UserDefaults.standard
+            if let tokenmintString = userDefaults.string(forKey: "tokenmint") {
+                let urlString2 = "\(self.baseUrlString)\(entryPoint)/connect-wallet?account_id=\(accountId)&public_key=\(publicKey)"
+                self.loadURL(urlString2)
+                print(urlString2)
                 
-                if let accountId = accountid, let publicKey = publickey, !accountId.isEmpty {
-                    // If account id is not empty, construct URL with connect-wallet path
-                    urlString = "\(self.baseUrlString)\(entryPoint)/connect-wallet?passport_access=\(encryptedToken)&account_id=\(accountId)&public_key=\(publicKey)"
-                    print(urlString)
-                } else {
+            } else {
+                // Handle the case where "tokenmint" is not found in UserDefaults
+                print("Missing tokenmint! Cannot construct URL.")
+            }
+            
+            
+        }else{
+            encryptAuthToken(authToken: authToken!) { encryptedToken in
+                DispatchQueue.main.async {
+                    var urlString: String
+                    
+//                    let userDefaults = UserDefaults.standard
+//                    userDefaults.set(encryptedToken, forKey: "tokenmint")
+//                    
+                    
                     // If account id is empty, construct URL with the specified path
                     urlString = "\(self.baseUrlString)\(entryPoint)?passport_access=\(encryptedToken)"
-                }
-                
-                if let url = URL(string: urlString) {
-                    self.webView.load(URLRequest(url: url))
-                } else {
-                    print("Error: Invalid URL")
+                    
+                    self.loadURL(urlString)
+                    
                 }
             }
         }
+            
+    
     }
-
+    
+    
+    func extractAccessToken(from optionalString: String?) -> String? {
+      guard let string = optionalString else { return nil }  // Handle nil case
+      
+      // Check if the string starts with "Optional(" and ends with ")"
+      if string.hasPrefix("Optional(\"") && string.hasSuffix("\")") {
+        // Remove the prefix and suffix
+        let startIndex = string.index(string.startIndex, offsetBy: 10) // Skip "Optional("
+        let endIndex = string.index(string.endIndex, offsetBy: -1)   // Skip ")"
+        let substring = String(string[startIndex..<endIndex])
+        return substring
+      } else {
+        // The string is not in the expected format
+        return nil  // Or handle this case differently (e.g., print an error)
+      }
+    }
+    
+    func loadURL(_ urlString: String) {
+      if let url = URL(string: urlString) {
+        self.webView.load(URLRequest(url: url))
+      } else {
+        print("Error: Invalid URL")
+      }
+    }
+    
 
     private func encryptAuthToken(authToken: String, completion: @escaping (String) -> Void) {
         // Prepare the request
-        let url = URL(string: UrlStringEncode)!
+        guard let url = URL(string: UrlStringEncode) else {
+            print("Invalid URL string: \(UrlStringEncode)")
+            return
+        }
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
@@ -122,32 +267,49 @@ public class ICCWebView: UIViewController, WKNavigationDelegate, WKScriptMessage
         // Prepare the request body
         let requestBody: [String: String] = [
             "authToken": authToken,
-            "name": name,
-            "email": email
+            "name": name!,
+            "email": email!
         ]
-        let jsonData = try! JSONSerialization.data(withJSONObject: requestBody)
-        request.httpBody = jsonData
+        
+        do {
+            let jsonData = try JSONSerialization.data(withJSONObject: requestBody, options: [])
+            request.httpBody = jsonData
+        } catch {
+            print("Error serializing JSON: \(error.localizedDescription)")
+            return
+        }
         
         // Make the request
         let task = URLSession.shared.dataTask(with: request) { data, response, error in
-            guard let data = data, error == nil else {
-                print("Error: \(error?.localizedDescription ?? "Unknown error")")
+            if let error = error {
+                print("Network error: \(error.localizedDescription)")
                 return
             }
             
-            // Parse the response
-            if let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any],
-               let statusCode = json["statusCode"] as? Int, statusCode == 200,
-               let responseData = json["data"] as? [String: Any],
-               let encryptedToken = responseData["token"] as? String {
-                // Call completion handler with encrypted token
-                completion(encryptedToken)
-            } else {
-                print("Error: Unable to parse response or token not found")
+            guard let data = data else {
+                print("No data received")
+                return
+            }
+            
+            do {
+                if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any],
+                   let statusCode = json["statusCode"] as? Int, statusCode == 200,
+                   let responseData = json["data"] as? [String: Any],
+                   let encryptedToken = responseData["token"] as? String {
+                    // Call completion handler with encrypted token
+                    completion(encryptedToken)
+                } else {
+                    print("Error: Unable to parse response or token not found")
+                }
+            } catch {
+                print("JSON parsing error: \(error.localizedDescription)")
             }
         }
         task.resume()
     }
+   
+    
+    
     
     public func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
         guard let url = navigationAction.request.url else {
@@ -197,6 +359,12 @@ public class ICCWebView: UIViewController, WKNavigationDelegate, WKScriptMessage
             controller.dismiss(animated: true, completion: nil)
         }
     }
+    
+    func retrieveEncryptedToken() -> String? {
+      let defaults = UserDefaults.standard
+      let encryptedToken = defaults.string(forKey: "encryptedToken")
+      return encryptedToken
+    }
 
     func handleDeepLink(_ url: URL) {
         // Check if the URL contains "mintbase.xyz"
@@ -216,6 +384,9 @@ public class ICCWebView: UIViewController, WKNavigationDelegate, WKScriptMessage
                         }
                     }
                 }
+                if let extractedPublicKey = publicKey {
+                      publicKey = encodePublicKey(extractedPublicKey)  // Encode the colon back
+                  }
                 
                 // Debugging output
                 print("Account ID: \(accountId ?? "N/A")")
@@ -223,7 +394,7 @@ public class ICCWebView: UIViewController, WKNavigationDelegate, WKScriptMessage
                 
                 // Inject JavaScript to handle the deeplink within the webview
                 let jsCommand = "handleDeepLink('\(url.absoluteString)')"
-                print(jsCommand)
+                print(url.absoluteString)
                 webView.evaluateJavaScript(jsCommand, completionHandler: nil)
                 
                 // Restart SDK operations with the extracted account_id and public_key
@@ -236,23 +407,25 @@ public class ICCWebView: UIViewController, WKNavigationDelegate, WKScriptMessage
         // Dismiss any presented view controllers, such as a Safari view controller
         self.presentedViewController?.dismiss(animated: true)
     }
-
-
     
-//    public func restartSDKAgain(entryPoint: PassportEntryPoint, accountid: String, publickey: String ) {
-//            encryptAuthToken(authToken: authToken) { encryptedToken in
-//                DispatchQueue.main.async {
-//                    var urlString: String
-//                    
-//                        urlString = "\(self.baseUrlString)\(entryPoint)/connect-wallet?passport_access=\(encryptedToken)&account_id=\(accountid)&public_key=\(publickey)"
-//                   
-//                    
-//                    if let url = URL(string: urlString) {
-//                        self.webView.load(URLRequest(url: url))
-//                    } else {
-//                        print("Error: Invalid URL")
-//                    }
-//                }
-//            }
-//        }
+    func encodePublicKey(_ publicKey: String) -> String {
+      return publicKey.replacingOccurrences(of: "%3A", with: "")
+    }
+    
+    func showPopup(title: String, message: String, buttons: [String] = ["OK"]) {
+       let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+
+       for buttonTitle in buttons {
+         let action = UIAlertAction(title: buttonTitle, style: .default) { _ in
+           // Handle button tap if needed (optional)
+         }
+         alertController.addAction(action)
+       }
+
+       self.present(alertController, animated: true, completion: nil)
+     }
+    
+    func presentAndHandleCallbacks(animated: Bool = true, completion: (() -> Void)? = nil) {
+        self.present(self, animated: animated, completion: completion)  // Present the ICCWebView
+      }
 }
